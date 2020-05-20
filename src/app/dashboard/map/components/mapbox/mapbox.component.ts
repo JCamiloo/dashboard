856189a3f103ca9c6716554acff6827af734c610
JCamiloo <jcamilo.osorio15@gmail.com>
2020-05-19 23:40:41 -0500

    mapbox added

--- src/app/dashboard/map/components/mapbox/mapbox.component.ts
+++ src/app/dashboard/map/components/mapbox/mapbox.component.ts
@@ -1,5 +1,6 @@
+import { environment } from './../../../../../environments/environment';
 import { Component, OnInit } from '@angular/core';
-
+import * as Mapboxgl from 'mapbox-gl';
 @Component({
   selector: 'app-mapbox',
   templateUrl: './mapbox.component.html',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapboxComponent implements OnInit {
 
+  map: Mapboxgl.Map;
+
   constructor() { }
 
   ngOnInit() {
+    this.renderMap();
   }
 
+  renderMap() {
+    Mapboxgl.accessToken = environment.mapbox_key;
+    this.map = new Mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-74.5, 40],
+      zoom: 9
+    });
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox_key: 'pk.eyJ1IjoiamNhbWlsb28iLCJhIjoiY2s2NDZiaDdxMGM3aTNqbXBtMnk3ZmQ5biJ9.bLy1nepm5WTdN8sqWiNd0w'
 };
